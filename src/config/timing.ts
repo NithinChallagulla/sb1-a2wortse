a8b5aea846734629b708c275085c0b312691774a
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2024-12-31 19:21:08 +0530

    Update timing.ts

--- src/config/timing.ts
+++ src/config/timing.ts
@@ -1,5 +1,4 @@
-// Convert January 1st, 2025 00:00:00 IST to UTC
-// IST is UTC+5:30, so we subtract 5 hours and 30 minutes from IST time
+
 export const TIMER_CONFIG = {
-  TARGET_DATE: new Date('2025-01-01T00:00:00+05:30').getTime(),
+  TARGET_DATE: Date.now() + 10 * 1000, // 10 seconds from now
 };
